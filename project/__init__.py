6514242a156580896dc0bcf8c907295cb6240790
ntoutloff <ntoutloff@gmail.com>
2025-12-28 21:05:59 -0600

    Set up to use different db for dev and prod

--- project/__init__.py
+++ project/__init__.py
@@ -14,12 +14,16 @@ def create_app():
     app = Flask(__name__)
     
     # Configuration
-    app.config['SECRET_KEY'] = 'your-secret-key-change-in-production'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB_HOST}:3306/{DB_NAME}'
-    app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 280, 'connect_args': {'connect_timeout': 5}}
-    
-    
+    if 'liveconsole' not in gethostname(): # If not running on pythonanywhere, use local sqlite db
+        app.config['SECRET_KEY'] = 'your-secret-key-change-in-production'
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    else:
+        app.config['SECRET_KEY'] = 'your-secret-key-change-in-production'
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = f'mysql+pymysql://{DB_USER}:{DB_PW}@{DB_HOST}:3306/{DB_NAME}'
+        app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {'pool_recycle': 280, 'connect_args': {'connect_timeout': 5}}
+
     # Initialize extensions with app
     db.init_app(app)
     
